f7852fff0d2179dff3e5bcc638eb082ab4c8ac04
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-01-22 15:40:28 +0530

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utlis.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req,res)=>{
     const {fullName,email,password} = req.body
@@ -37,15 +39,24 @@ export const signup = async (req,res)=>{
 
 
         if (newUser){
-            generateToken(newUser._id,res)
-            await newUser.save()
+            // generateToken(newUser._id,res)
+            // await newUser.save()
+
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id,res);
 
             res.status(201).json({
                 _id:newUser._id,
                 fullName:newUser.fullName,
                 email:newUser.email,
                 pfp: newUser.pfp,
-            })
+            });
+
+            try {
+                await sendWelcomeEmail(savedUser.email,savedUser.fullName, ENV.CLIENT_URL)
+            } catch (error) {
+                console.log("Failed to send welcome email",error);
+            }
         }else{
             res.status(400).json({message:"Invalid user data"})
         }
