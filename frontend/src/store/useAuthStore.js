e2ece21dfbbf0a043b4f4f98eb2f4720a4afe211
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-02-19 23:12:38 +0530

    backend url updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 // import { logout, updateProfile } from "../../../backend/src/controllers/auth.controller";
 import {io} from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chatifyyy.onrender.com/";
 
 export const useAuthStore = create((set,get)=>({
    
