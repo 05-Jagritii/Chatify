87b1bf0f64b162680cf1156c92bc6b97f4be1b34
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-02-19 23:26:38 +0530

    base url updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://chatifyyy.onrender.com/api" : "/api",
-    withCredentials: true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://chatifyyy.onrender.com/api",
+  withCredentials: true,
+});
