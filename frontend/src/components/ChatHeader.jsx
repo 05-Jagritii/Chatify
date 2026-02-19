e244cce40f707bd3ebe2e3d8fc6284ba08878d2c
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-02-19 21:00:22 +0530

    online users implemented

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,48 +1,48 @@
-import React, { useEffect } from 'react'
-import { useChatStore } from '../store/useChatStore';
-import {XIcon} from 'lucide-react';
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { XIcon } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 
 
 function ChatHeader() {
+  const { setSelectedUser, selectedUser } = useChatStore();
+  const { onlineUsers = [] } = useAuthStore();
 
-    const {setSelectedUser, selectedUser} = useChatStore(); 
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") setSelectedUser(null);
+    };
 
-    useEffect(()=> {
-        const handleEscKey = (event) => {
-            if (event.key === "Escape") setSelectedUser(null);
-        };
+    window.addEventListener("keydown", handleEscKey);
 
-        window.addEventListener("keydown",handleEscKey);
-
-        return () => window.removeEventListener("keydown",handleEscKey);
-    },[setSelectedUser]);
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [setSelectedUser]);
 
   return (
-    <div className='flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1'>
-
-        <div className='flex items-center space-x-3'>
-
-            <div className='avatar online'>
-
-                <div className='w-12 rounded-full'>
-
-                    <img src= {selectedUser.pfp || "/avatar.png"} alt={selectedUser.fullName} />
-
-                </div>
-
-            </div>
-
-            <div>
-                <h3 className='text-slate-200 font-medium'>{selectedUser.fullName}</h3>
-                <p className='text-slate-400 text-sm'>Online</p>
-            </div>
-
+    <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1">
+      <div className="flex items-center space-x-3">
+        <div className={`avatar ${onlineUsers.includes(selectedUser._id) ? "online" : "offline"}`}>
+          <div className="w-12 rounded-full">
+            <img
+              src={selectedUser.pfp || "/avatar.png"}
+              alt={selectedUser.fullName}
+            />
+          </div>
         </div>
 
-        <button onClick={()=> setSelectedUser(null)}>
-            <XIcon className='w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer'/>
-        </button>
+        <div>
+          <h3 className="text-slate-200 font-medium">
+            {selectedUser.fullName}
+          </h3>
+          <p className="text-slate-400 text-sm">
+            {onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
+          </p>
+        </div>
+      </div>
 
+      <button onClick={() => setSelectedUser(null)}>
+        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+      </button>
     </div>
   );
 }
